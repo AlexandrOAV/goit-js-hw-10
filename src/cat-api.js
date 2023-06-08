cb630083898536fb5a9c14dfa916608a15c43f79
AlexandrOAV <a0617011995@gmail.com>
2023-06-08 18:02:57 +0300

    task end new

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,53 +1,8 @@
 import Notiflix from 'notiflix';
 Notiflix.Notify.init({
-  width: '360px',
   position: 'center-center', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
-  distance: '10px',
-  opacity: 1,
-  borderRadius: '5px',
-  rtl: false,
-  timeout: 10000,
-  messageMaxLength: 110,
-  backOverlay: false,
-  backOverlayColor: 'rgba(0,0,0,0.5)',
-  plainText: true,
-  showOnlyTheLastOne: false,
+  timeout: 5000,
   clickToClose: true,
-  pauseOnHover: true,
-
-  ID: 'NotiflixNotify',
-  className: 'notiflix-notify',
-  zindex: 4001,
-  fontFamily: 'Quicksand',
-  fontSize: '16px',
-  cssAnimation: true,
-  cssAnimationDuration: 1000,
-  cssAnimationStyle: 'zoom', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
-  closeButton: false,
-  useIcon: false,
-  useFontAwesome: false,
-  fontAwesomeIconStyle: 'basic', // 'basic' - 'shadow'
-  fontAwesomeIconSize: '34px',
-
-  success: {
-    background: '#32c682',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-success',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-check-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(50,198,130,0.2)',
-  },
-
-  failure: {
-    background: '#ff5549',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-failure',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-times-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(255,85,73,0.2)',
-  },
 });
 const KEY = 'live_rZzjgKrmWzeonY3YGx515qAI1Mr4ZcuaWUnHyhXO3XOAZGOFIhcABMpmmZ7JbhTK';  
 export function fetchBreeds() {
--- src/index.js
+++ src/index.js
@@ -1,62 +1,17 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api'
 import Notiflix from 'notiflix';
-Notiflix.Notify.init({
-  width: '360px',
-  position: 'right-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
-  distance: '10px',
-  opacity: 1,
-  borderRadius: '5px',
-  rtl: false,
-  timeout: 4000,
-  messageMaxLength: 110,
-  backOverlay: false,
-  backOverlayColor: 'rgba(0,0,0,0.5)',
-  plainText: true,
-  showOnlyTheLastOne: false,
-  clickToClose: false,
-  pauseOnHover: true,
 
-  ID: 'NotiflixNotify',
-  className: 'notiflix-notify',
-  zindex: 4001,
-  fontFamily: 'Quicksand',
-  fontSize: '16px',
-  cssAnimation: true,
-  cssAnimationDuration: 400,
-  cssAnimationStyle: 'fade', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
-  closeButton: false,
-  useIcon: false,
-  useFontAwesome: false,
-  fontAwesomeIconStyle: 'basic', // 'basic' - 'shadow'
-  fontAwesomeIconSize: '34px',
-
-  success: {
-    background: '#32c682',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-success',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-check-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(50,198,130,0.2)',
-  },
-
-  failure: {
-    background: '#ff5549',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-failure',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-times-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(255,85,73,0.2)',
-  },
-});
 const selectEl = document.querySelector('.breed-select');
 const divInfoEl = document.querySelector('.cat-info');
 const loaderEl = document.querySelector('.loader');
 
 fetchBreeds().then(catsArray => {
   addOption(catsArray);
- })
+})
+  .catch(error => {
+    Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+  });
+  
 function addOption(array) {
 const selectHtml = array.map(({id, name}) => {
       return `<option value=${id}>${name}</option>`
@@ -70,12 +25,19 @@ selectEl.addEventListener('change', event => {
   const breedId = event.target.value;
   loaderEl.classList.remove('hidden');
   fetchCatByBreed(breedId).then(cats => {
-  const imgURL = cats.url;
-  const infoCat = cats.breeds[0].description;
-  const temperamentCat = cats.breeds[0].temperament;
-  const nameHeader = cats.breeds[0].name;
-  addInfoCat({ imgURL, infoCat, temperamentCat, nameHeader });  
-  });
+    const imgURL = cats.url;
+    const infoCat = cats.breeds[0].description;
+    const temperamentCat = cats.breeds[0].temperament;
+    const nameHeader = cats.breeds[0].name;
+    addInfoCat({ imgURL, infoCat, temperamentCat, nameHeader });
+  })
+  .catch (error => {
+    loaderEl.classList.add('hidden');
+    divInfoEl.innerHTML = '';
+    Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+  }
+  
+  );
 })
 
 
